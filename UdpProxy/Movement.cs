a96bbc975c99637c1dbbea281c1a1db31e47b185
Mikkel Yde Engquist <mikk9553@edu.easj.dk>
2022-05-05 15:32:28 +0200

    it works?

---
+++ UdpProxy/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpProxy
+{
+    public class Movement
+    {
+        public DateTime timeStamp { get; set; }
+        public string movement { get; set; }
+
+
+        public Movement()
+        {
+        }
+
+        public Movement(string movement, DateTime timestamp)
+        {
+            this.movement = movement;
+            timeStamp = timestamp;
+        }
+    }
+}
--- UdpProxy/Program.cs
+++ UdpProxy/Program.cs
@@ -5,15 +5,22 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Diagnostics;
+using System.Net.Http.Json;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace UdpProxy
 {
     class Program
     {
-        private static string URL = "https://restexcuses.azurewebsites.net/api/Excuse";
+        private static string URL = "https://restexcuses.azurewebsites.net/api/Movement";
 
-        static void Main(string[] args)
+        //public static async void Post(HttpContent post)
+        //{
+        //    HttpClient http = new HttpClient();
+        //    await http.PostAsync(URL, post);
+        //}
+        static async Task Main(string[] args)
         {
             Console.WriteLine("UDP Listener");
             using (UdpClient socket = new UdpClient())
@@ -28,17 +35,18 @@ namespace UdpProxy
                         IPEndPoint from = null;
 
                         byte[] data = socket.Receive(ref from);
-                        //string timeStamp = GetTimestamp(DateTime.UtcNow);
-                        //DateTimeOffset now = (DateTimeOffset)DateTime.UtcNow;
 
                         string stringData = Encoding.UTF8.GetString(data);
 
+                        Movement movement = new Movement(stringData, DateTime.Now);
                         Console.WriteLine("Server received: " + stringData + " From " + from.Address);
 
-                        HttpContent content = new StringContent(stringData, Encoding.UTF8, "application/json");
+                        //HttpContent content = new StringContent(stringData, Encoding.UTF8, "application/json");
 
-                        client.PostAsync(URL, content);
-                        Thread.Sleep(10000);
+                        var response = await client.PostAsJsonAsync(URL, movement);
+                        //Post(content);
+                        //client.PostAsync(URL, content);
+                        //Thread.Sleep(1000);
 
                     }
                 }
